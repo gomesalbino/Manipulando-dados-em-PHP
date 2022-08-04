7e9e20a3c2739ec948562867e9a0e3cab36ca488
Albino Gomes <gomesalbino26@outlook.com>
2022-08-04 20:25:39 -0300

    Adicionando tipos de dados

---
+++ operacao-matematico.php
@@ -0,0 +1,25 @@
+<?php
+
+$idadeAtual = 26;
+
+$minhaIdadeDaquiHaDezAnos = $idadeAtual + 10;
+
+//echo $minhaIdadeDaquiHaDezAnos;
+
+$minhaIdade = 20;
+$minhaIdadeHa5anosAtras = $minhaIdade - 5;
+//echo $minhaIdadeHa5anosAtras;
+
+$somaValor = 20 + 20;
+
+$multiplica = 52 * 33;
+
+$divisao = 45 / 5;
+
+$subtracao = 25 - 12;
+
+$doisAoCubo = 2 ** 3;
+
+$restoDaDivisao = 11 % 5;
+
+echo $restoDaDivisao;
---
+++ tipos-de-dados.php
@@ -0,0 +1,28 @@
+<?php
+
+$salario  = 1000.20;
+
+//echo gettype($salario);
+
+$minhdaIdade =  27;
+//echo gettype($minhdaIdade);
+
+$temCarro = true;
+
+//echo gettype($temCarro);
+
+$naoTemCarro = false;
+
+//echo gettype($naoTemCarro);
+
+$meuNome = "Albino Gomes";
+
+//echo gettype($meuNome);
+
+$divisao = 25/3;
+
+//echo gettype($divisao);
+
+echo ($divisao);
+
+
