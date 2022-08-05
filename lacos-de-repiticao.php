0348e75a80d21b89fcb6990062fe3fcb9d24ff4b
Albino Gomes <gomesalbino26@outlook.com>
2022-08-05 13:55:47 -0300

    Adcionando laços de repitições

---
+++ lacos-de-repiticao.php
@@ -0,0 +1,58 @@
+
+<?php
+
+// $contador = 1 ;
+
+// while ($contador <= 15){
+//     echo $contador . PHP_EOL;
+//     $contador = $contador + 1;
+// }
+
+//Contar os numeros inversos
+
+// $contador = 15 ;
+
+// while ($contador >= 1){
+//     echo $contador . PHP_EOL;
+//     $contador = $contador - 1;
+// }
+
+
+//Agora vamos executar a mesma verificação usando o for.
+
+for ($contador = 1; $contador <= 15; $contador++){
+    echo $contador . PHP_EOL;
+}
+
+
+//Pular numero 13 na contagem
+
+
+// for ($contador = 1; $contador <= 15; $contador++){
+//     if($contador == 13){
+//         continue;
+//     }
+
+//      echo $contador . PHP_EOL;
+    
+// }
+
+//Pular numero 13 na contagem, em outra maneira
+
+// for ($contador = 1; $contador <= 15; $contador++){
+//     if($contador != 13){
+//         echo $contador . PHP_EOL;
+//     }
+
+// }
+
+
+//Forçar a saida do laço
+
+// for ($contador = 1; $contador <= 15; $contador++){
+//     if ($contador == 13){
+//         break;
+//     }
+//     echo $contador . PHP_EOL;
+// }
+
