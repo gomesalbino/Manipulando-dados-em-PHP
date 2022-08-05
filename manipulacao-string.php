7de609cf306b2be9b0dacc4cc721daf799290caa
Albino Gomes <gomesalbino26@outlook.com>
2022-08-05 00:38:57 -0300

    Adicionando a manipulação de string

---
+++ manipulacao-string.php
@@ -0,0 +1,39 @@
+
+<?php
+
+$minhaIdade = 15;
+
+//echo 'Minha Idade é $minhaIdade';
+
+//echo 'minha idade é'. $minhaIdade . 'anos';
+
+/*
+$nome = "Albino Gomes";
+
+echo "Olá meu nome é $nome" .PHP_EOL;
+
+echo "Tenho $minhaIdade anos" .PHP_EOL;
+
+echo "Comecei a estudadar programação desde Outubro de ano passado!";
+*/
+/*
+$nome = "Albino Gomes";
+
+echo "Olá meu nome é $nome \n";
+
+echo "Tenho $minhaIdade anos \n";
+
+echo "Comecei a estudadar programação desde Outubro de ano passado! \n";
+*/
+
+$nome = "Albino Gomes";
+
+echo "Olá meu nome é $nome" .PHP_EOL;
+
+echo "Tenho $minhaIdade anos " .PHP_EOL;
+
+echo "Comecei a estudadar programação desde Outubro de ano passado!" .PHP_EOL;
+
+echo "Só pode entrar se estiver maior de 18 anos" .PHP_EOL;
+
+echo "Você tem $minhaIdade anos pode entrar!!";
